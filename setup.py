020b230e1f4b35ef0218dc89b83863393c9824cc
Antti Leppä <antti.leppa@foyt.fi>
2016-08-01 17:07:35 +0300

    Converting apt-history to executable

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 setup(
   name = 'apt-history',
   packages = ['apt-history'],
-  version = '0.2.4',
+  version = '0.2.5',
   description = 'apt history script',
   author = 'Antti Leppa',
   author_email = 'antti.leppa@metatavu.fi',
   url = 'https://github.com/Metatavu/apt-history',
-  download_url = 'https://github.com/Metatavu/apt-history/tarball/0.2.4', 
+  download_url = 'https://github.com/Metatavu/apt-history/tarball/0.2.5', 
   keywords = ['apt'],
   classifiers = [],
   entry_points={
     'console_scripts': [
-      'apt-history=main:main',
+      'apt-history=apt-history:main',
     ],
   },
 )
