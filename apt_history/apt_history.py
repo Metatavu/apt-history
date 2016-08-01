919011b5817a54e52c6975c1f2f9f46ffb33b99d
Antti Leppä <antti.leppa@foyt.fi>
2016-08-01 17:22:12 +0300

    Converting apt-history to executable

--- apt_history/apt_history.py
+++ apt_history/apt_history.py
@@ -1,9 +1,5 @@
 #! /usr/bin/env python3
-import argparse
 import re
-import os
-import copy
-from subprocess import call
 
 class PackageChangeSet:
     def __init__(self, originalDate, originalCommand, packageChanges):
@@ -168,43 +164,3 @@ def abbreviate(string):
         return string[:17] + '...'
     else:
         return string
-            
-def main():
-    parser = argparse.ArgumentParser(description='APT history')
-    parser.add_argument('command', help='command to run')
-    parser.add_argument('--rollback-count', type=int, default=1, help='number of change sets to rollback')
-    parser.add_argument('--simulate', dest='simulate', action='store_true', help='do not actually run commands')
-    parser.add_argument('--log-file', default='/var/log/apt/history.log', help='apt logfile to use')
-    parser.set_defaults(simulate=False)
-    
-    args = parser.parse_args()
-    
-    packageChangeSets = []
-    
-    with open(args.log_file) as log_file:
-        lines = iter(log_file.readlines())
-        while 1:
-            packageChangeSet = readNextPackageChangeSet(lines)
-            if not packageChangeSet:
-                break
-            packageChangeSets.append(packageChangeSet)
-        
-    if args.command == 'rollback':
-        rollbackSets = []
-        callEnv = copy.copy(os.environ);
-        callEnv.update({
-          'DEBIAN_FRONTEND': 'noninteractive'
-        });
-        
-        print(callEnv);
-        
-        for rollbackIndex in range(len(packageChangeSets) - args.rollback_count, len(packageChangeSets)):
-            rollbackSets.append(packageChangeSets[rollbackIndex].createrollbackSet())
-    
-        for rollbackSet in rollbackSets:
-            print('Rollback for %s (%s)' % (abbreviate(rollbackSet.originalCommand), rollbackSet.originalDate))
-            command = ['apt-get', '-yq', '--allow-downgrades', '-o Dpkg::Options::="--force-confnew"', '-o Dpkg::Options::="--force-confold"', '-o Dpkg::Options::="--force-confdef"', 'install']
-            command.extend(rollbackSet.getAptArgs())
-            print('  > Running %s...' % (' '.join(command)))
-            if not args.simulate:
-                call(command, env=callEnv)
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
   name = 'apt-history',
   packages = ['apt_history'],
-  version = '0.2.9',
+  version = '0.2.10',
   description = 'apt history script',
   author = 'Antti Leppa',
   author_email = 'antti.leppa@metatavu.fi',
   url = 'https://github.com/Metatavu/apt-history',
-  download_url = 'https://github.com/Metatavu/apt-history/tarball/0.2.9', 
+  download_url = 'https://github.com/Metatavu/apt-history/tarball/0.2.10', 
   keywords = ['apt'],
   classifiers = [],
   entry_points={
